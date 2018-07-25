23c30454540468631165198ca8e1ddee510b3f2d
g17e4476 <g17e4476@campus.ru.ac.za>
2018-07-25 11:17:58 +0200

    WORKING :D

--- Prac02_Classwork/ChangeCalculator/Program.cs
+++ Prac02_Classwork/ChangeCalculator/Program.cs
@@ -10,49 +10,62 @@ namespace ChangeCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter an amount: ");
+            Console.WriteLine("Please enter an amount: ");              //user input
             string cash = Console.ReadLine();
             
-            bool money = Double.TryParse(cash, out Double dcash);
+            bool money = Double.TryParse(cash, out Double dcash);       //input validation check
             if(money == true)
             {
-                Console.WriteLine("great input!!");
+                Console.WriteLine("great input!!");                     //user feedback
 
             }
             else {
-                Console.WriteLine("It makes no sense!!");
+                Console.WriteLine("It makes no sense!!");               //rejection
             }
-            int[] Currency = new int[] { 200, 100, 50, 20, 10, 5, 2, 1, 50, 20, 10, 5, 2, 1 };
+            int[] Currency = new int[] { 200, 100, 50, 20, 10, 5, 2, 1, 50, 20, 10, 5, 2, 1 }; //Currency
 
-            double rands = System.Math.Floor(dcash);
-            double cents = dcash - rands;
-
-            cents = System.Math.Floor(cents * 100);
-            int cent = Convert.ToInt16(cents);
-            int rand = Convert.ToInt32(rands);
-            int i = 0;
+            double rands = System.Math.Floor(dcash);                    //Obtains the rand
+            double cents = dcash - rands;                               //Calculates the cents
+            cents = System.Math.Round(cents, 3);
+            cents = System.Math.Floor(cents * 100);                     //Takes first two cent digits
+            int cent = Convert.ToInt16(cents);                          // conversion to int
+            int rand = Convert.ToInt32(rands);                          // conversion to int
+            int i = 0;                                                  // variable declaration
             string item = "note";
             int nrn = 0;
             int ncn = 0;
-            while (rand != 0)
+            while (rand != 0)                               //While there are still rands to be calculated
             {
                  
-nrn = rand % Currency[i];
-                rand = rand - nrn * Currency[i];
-                if (i > 4)
+                nrn = rand / Currency[i];                   // Notes needed
+                rand = rand - nrn * Currency[i];            // diminish rands
+                if (i > 4)                                  // after fourth value only coins remain in currency spectrum
                 {
-                     item = "coin";
+                     item = "coin";                         // set currency item to coin
                 }
-                Console.WriteLine("Return " + nrn + " x R" + Currency[i] + " " + item + "(s)");
-                i++;
+                if(nrn != 0)                                // only if there is currency needed will it display it
+                {
+                    Console.WriteLine("Return " + nrn + " x R" + Currency[i] + " " + item + "(s)");
+                    
+                }
+                i++;        // increment i 
             }
-            while(cent != 0)
+            i = 8;
+            while(cent != 0)                        // cents still need processing
             {
-                ncn = cent % Currency[i];
+                
+                ncn = cent / Currency[i];
                 cent = cent - ncn * Currency[i];
-                Console.WriteLine("Return " + ncn + " x R" + Currency[i] + " " + item + "(s)");
+                if(ncn != 0)
+                {
+                    Console.WriteLine("Return " + ncn + " x " + Currency[i] + "c" + " " + item + "(s)");
+                }
+                
                 i++;
             }
+
+            
+            
             
 
           
